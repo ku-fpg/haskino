6b9ae66e3ecbf30b5d00db624bfde79c31c576fb
Mark Grebe <mark.grebe@ku.edu>
2015-06-18 12:39:22 -0500

    Update Servo sample

--- System/Hardware/DeepArduino/Parts/Servo.hs
+++ System/Hardware/DeepArduino/Parts/Servo.hs
@@ -21,6 +21,7 @@ module System.Hardware.DeepArduino.Parts.Servo(
 
 import Data.Bits     (shiftR, (.&.))
 import Data.Maybe    (fromMaybe)
+import Control.Monad (when)
 import Data.Word     (Word16)
 
 import System.Hardware.DeepArduino
@@ -34,6 +35,9 @@ data Servo = Servo { servoPin :: Pin      -- ^ The pin that controls the servo
                    , maxPulse :: Int      -- ^ Pulse-width (microseconds) for the maximum (typically 180-degree) angle.
                    }
 
+errorServo :: Servo
+errorServo = Servo { servoPin = digital 0, minPulse = 0, maxPulse = 0}
+
 -- | Create a servo motor instance. The default values for the min/max angle pulse-widths, while typical,
 -- may need to be adjusted based on the specs of the actual servo motor. Check the data-sheet for your
 -- servo to find the proper values. The default values of @544@ and @2400@ microseconds are typical, so you might
@@ -41,7 +45,28 @@ data Servo = Servo { servoPin :: Pin      -- ^ The pin that controls the servo
 attach :: Pin            -- ^ Pin controlling the servo. Should be a pin that supports SERVO mode.
        -> Maybe Word16   -- ^ Pulse-width (in microseconds) for the minumum 0-degree angle. Default: @544@.
        -> Maybe Word16   -- ^ Pulse-width (in microseconds) for the maximum, typically 180-degree, angle. Default: @2400@.
-       -> (Servo, Arduino ())
+       -> Arduino Servo
+attach p mbMin mbMax
+  | Just m <- mbMin, m < 0
+  = do die "Servo.attach: minimum pulse width must be positive" ["Received: " ++ show m]
+       return errorServo
+  | Just m <- mbMax, m < 0
+  = do die "Servo.attach: maximum pulse width must be positive" ["Received: " ++ show m]
+       return errorServo
+  | True
+  = do let minPulse = fromMaybe 544  mbMin
+           maxPulse = fromMaybe 2400 mbMax
+       debug $ "Attaching servo on pin: " ++ show p ++ " with parameters: " ++ show (minPulse, maxPulse)
+       when (minPulse >= maxPulse) $ die "Servo.attach: min pulse duration must be less than max pulse duration"
+                                         [ "Received min-pulse: " ++ show minPulse
+                                         , "Received max-pulse: " ++ show maxPulse
+                                         ]
+       setPinMode p SERVO
+       return Servo { servoPin = p
+                    , minPulse = fromIntegral $ fromMaybe 544  mbMin
+                    , maxPulse = fromIntegral $ fromMaybe 2400 mbMax
+                    }
+{-
 attach p mbMin mbMax = 
     (Servo { servoPin = p , 
              minPulse = fromIntegral $ minPulse, 
@@ -56,14 +81,75 @@ attach p mbMin mbMax =
     max = fromMaybe defaultMax mbMax
     minPulse = if min > max then defaultMin else min
     maxPulse = if min > max then defaultMax else max
+-}
+-- | Set the angle of the servo. The argument should be a number between 0 and 180,
+-- indicating the desired angle setting in degrees.
+setAngle :: Servo -> Int -> Arduino ()
+setAngle Servo{servoPin, minPulse, maxPulse} angle
+  | angle < 0 || angle > 180
+  = die "Servo.setAngle: angle must be between 0 and 180." ["Received: " ++ show angle]
+  | True
+  = do let duration = minPulse + ((maxPulse - minPulse) * angle) `div` 180
+       debug $ "Setting servo on pin: " ++ show servoPin ++ " " ++ show angle ++ " degrees, via a pulse of " ++ show duration ++ " microseconds."
+       -- In arduino, the most we can send is 16383; not that a servo should need such a large value, but
+       -- just in case
+       when (duration >= 16383) $ die "Servo.setAngle angle setting: out-of-range."
+                                      [ "Servo pin         : " ++ show servoPin
+                                      , "Angle required    : " ++ show angle
+                                      , "Min pulse duration: " ++ show minPulse
+                                      , "Max pulse duration: " ++ show maxPulse
+                                      , "Duration needed   : " ++ show duration
+                                      , "Exceeds max value : 16383"
+                                      ]
+       analogWrite servoPin (fromIntegral duration)
+
+
+
+{-
+attach :: Pin            -- ^ Pin controlling the servo. Should be a pin that supports SERVO mode.
+       -> Maybe Int      -- ^ Pulse-width (in microseconds) for the minumum 0-degree angle. Default: @544@.
+       -> Maybe Int      -- ^ Pulse-width (in microseconds) for the maximum, typically 180-degree, angle. Default: @2400@.
+       -> Arduino Servo
+attach p mbMin mbMax
+  | Just m <- mbMin, m < 0
+  = die "Servo.attach: minimum pulse width must be positive" ["Received: " ++ show m]
+  | Just m <- mbMax, m < 0
+  = die "Servo.attach: maximum pulse width must be positive" ["Received: " ++ show m]
+  | True
+  = do let minPulse = fromMaybe 544  mbMin
+           maxPulse = fromMaybe 2400 mbMax
+       debug $ "Attaching servo on pin: " ++ show p ++ " with parameters: " ++ show (minPulse, maxPulse)
+       when (minPulse >= maxPulse) $ die "Servo.attach: min pulse duration must be less than max pulse duration"
+                                         [ "Received min-pulse: " ++ show minPulse
+                                         , "Received max-pulse: " ++ show maxPulse
+                                         ]
+       setPinMode p SERVO
+       (ip, _) <- convertAndCheckPin "Servo.attach" p SERVO
+       return Servo { servoPin = ip
+                    , minPulse = fromMaybe 544  mbMin
+                    , maxPulse = fromMaybe 2400 mbMax
+                    }
 
 -- | Set the angle of the servo. The argument should be a number between 0 and 180,
 -- indicating the desired angle setting in degrees.
 setAngle :: Servo -> Int -> Arduino ()
 setAngle Servo{servoPin, minPulse, maxPulse} angle
   | angle < 0 || angle > 180
-  = debug $ "Invalid servo angle " ++ show angle ++ " for Servo on pin " ++ show servoPin
+  = die "Servo.setAngle: angle must be between 0 and 180." ["Received: " ++ show angle]
   | True
   = do let duration = minPulse + ((maxPulse - minPulse) * angle) `div` 180
        debug $ "Setting servo on pin: " ++ show servoPin ++ " " ++ show angle ++ " degrees, via a pulse of " ++ show duration ++ " microseconds."
-       analogWrite servoPin (fromIntegral $ minimum [duration,16383])
+       -- In arduino, the most we can send is 16383; not that a servo should need such a large value, but
+       -- just in case
+       when (duration >= 16383) $ die "Servo.setAngle angle setting: out-of-range."
+                                      [ "Servo pin         : " ++ show servoPin
+                                      , "Angle required    : " ++ show angle
+                                      , "Min pulse duration: " ++ show minPulse
+                                      , "Max pulse duration: " ++ show maxPulse
+                                      , "Duration needed   : " ++ show duration
+                                      , "Exceeds max value : 16383"
+                                      ]
+       let msb = fromIntegral $ (duration `shiftR` 7) .&. 0x7f
+           lsb = fromIntegral $ duration .&. 0x7f
+       send $ AnalogPinWrite servoPin lsb msb
+-}
--- System/Hardware/DeepArduino/SamplePrograms/Servo.hs
+++ System/Hardware/DeepArduino/SamplePrograms/Servo.hs
@@ -14,6 +14,7 @@
 module System.Hardware.Arduino.SamplePrograms.Servo where
 
 import Control.Monad       (forever)
+import Control.Monad.Trans (liftIO)
 import Data.Char           (toLower)
 
 import System.Hardware.DeepArduino
@@ -36,54 +37,33 @@ import System.Hardware.DeepArduino.Parts.Servo
 --
 --  <<http://github.com/LeventErkok/hArduino/raw/master/System/Hardware/Arduino/SamplePrograms/Schematics/Servo.png>>
 servo :: IO ()
-servo = do
-    conn <- openArduino True "/dev/cu.usbmodem1421"
-    -- Create the Servo structure and get the servo init function
-    let (s,init) = attach (digital 9) (Just 600) (Just 2400)
-    -- Send the servoinit to the arduino
-    send conn init
-    forever (demo conn s)
- where 
-    demo c s = do 
-        putStr "Enter l, r or the desired servo angle: "
-        a <- getLine
-        let ang = read a :: Int
-            move a = do
-                setAngle s a
-                delay 100
-            ms =  case (map toLower a, ang) of
-                     ("l", _) -> mapM_ move [0 .. 180]
-                     ("r", _) -> mapM_ move [180, 179 .. 0]
-                     (_,  v) | 0 <= v && v <= 180 -> setAngle s v
-                     (_, _)  -> return ()
-        send c ms
-
+servo = withArduino False "/dev/cu.usbmodem1421" $ do
+            s <- attach (digital 9) (Just 600) (Just 2400)
+            forever (demo s)
+ where demo s = do liftIO $ putStr "Enter l, r or the desired servo angle: "
+                   a <- liftIO getLine
+                   case (map toLower a, reads a) of
+                    ("l", _) -> mapM_ move [0 .. 180]
+                    ("r", _) -> mapM_ move [180, 179 .. 0]
+                    (_,  [(v, "")]) | 0 <= v && v <= 180
+                             -> setAngle s v
+                    _        -> liftIO $ putStrLn "Invalid entry."
+         where move a = setAngle s a >> delay 100
 
 -- | Control a servo, as guided by the input read from a potentiometer. The set-up is similar to the 'servo' example
 -- above, except instead of querying the user for the angle, we use the readings from a potentiometer connected to
 -- analog input number 2. We used a 10 KOhm potentiometer, but other pots would work just as well too:
 --
 --  <<http://github.com/LeventErkok/hArduino/raw/master/System/Hardware/Arduino/SamplePrograms/Schematics/ServoAnalog.png>>
-servoAnalog :: IO ()
-servoAnalog = do
-    conn <- openArduino False "/dev/cu.usbmodem1421"
-    -- Create the Servo structure and get the servo init function
-    let (s,init) = attach (digital 9) (Just 600) (Just 2400)
-        pot = analog 2
-    -- Send the servoinit to the arduino
-    send conn init
-    -- Setup the pot input
-    send conn $ do
-      setPinMode pot ANALOG
-      analogReport pot True
-    putStrLn "Adjust the potentiometer to control the servo!"
-    forever $ send conn (demo s pot)
- where 
-    demo s pot = do 
-          v <- analogRead pot
-          setAngle s (fromIntegral $ cvt v)
-          delay 100
-    -- Analog input will be from 0 to 1023; convert it to
-    -- angles, mapping 1023 to 0-degrees, and 0 to 180
-    cvt i = ((1023-i) * 180) `div` 1023
-
+servoAnalog = withArduino False "/dev/cu.usbmodem1421" $ do
+                 s <- attach (digital 9) (Just 600) (Just 2400)
+                 setPinMode pot ANALOG
+                 liftIO $ putStrLn "Adjust the potentiometer to control the servo!"
+                 forever (demo s)
+ where pot = analog 2
+       demo s = do v <- analogRead pot
+                   setAngle s (fromIntegral $ cvt v)
+                   delay 100
+       -- Analog input will be from 0 to 1023; convert it to
+       -- angles, mapping 1023 to 0-degrees, and 0 to 180
+       cvt i = ((1023-i) * 180) `div` 1023
