256b71dc74b24a84514c9bf62acab9c8584b1511
Mark Grebe <mark.grebe@ku.edu>
2015-09-02 20:11:52 -0500

    Added bc expr command handlers

--- firmware/HaskinoFirmware/HaskinoBoardControl.cpp
+++ firmware/HaskinoFirmware/HaskinoBoardControl.cpp
@@ -2,6 +2,7 @@
 #include "HaskinoBoardControl.h"
 #include "HaskinoComm.h"
 #include "HaskinoCommands.h"
+#include "HaskinoExpr.h"
 #include "HaskinoFirmware.h"
 #include "HaskinoScheduler.h"
 #include "SoftReset.h"
@@ -10,6 +11,9 @@ static bool handleSetPinMode(int size, const byte *msg);
 static bool handleDelayMillis(int size, const byte *msg);
 static bool handleDelayMicros(int size, const byte *msg);
 static bool handleSystemReset(int size, const byte *msg);
+static bool handleSetPinModeE(int size, const byte *msg);
+static bool handleDelayMillisE(int size, const byte *msg);
+static bool handleDelayMicrosE(int size, const byte *msg);
 
 bool parseBoardControlMessage(int size, const byte *msg, byte *local)
     {
@@ -26,6 +30,15 @@ bool parseBoardControlMessage(int size, const byte *msg, byte *local)
             break;
         case BC_CMD_SYSTEM_RESET:
             return handleSystemReset(size, msg);
+            break;
+        case BC_CMD_SET_PIN_MODE_E:
+            return handleSetPinModeE(size, msg);
+            break;
+        case BC_CMD_DELAY_MILLIS_E:
+            return handleDelayMillisE(size, msg);
+            break;
+        case BC_CMD_DELAY_MICROS_E:
+            return handleDelayMicrosE(size, msg);
             break;
         }
     return false;
@@ -57,6 +70,38 @@ static bool handleDelayMicros(int size, const byte *msg)
     {
     unsigned int micros;
     memcpy(&micros, &msg[1], 2);
+    delayMicroseconds(micros);
+    return false;
+    }
+
+static bool handleSetPinModeE(int size, const byte *msg)
+    {
+    byte *expr = (byte *) &msg[1];
+    byte pinNo = evalWord8Expr(&expr);
+    pinMode(pinNo, msg[2]);
+    return false;
+    }
+
+static bool handleDelayMillisE(int size, const byte *msg)
+    {
+    byte *expr = (byte *) &msg[1];
+    uint32_t millis = evalWord32Expr(&expr);
+
+    if (isRunningTask())
+        {
+        delayRunningTask(millis);
+        }
+    else 
+        {
+        delay(millis);
+        }
+    return true;
+    }
+
+static bool handleDelayMicrosE(int size, const byte *msg)
+    {
+    byte *expr = (byte *) &msg[1];
+    uint32_t micros = evalWord16Expr(&expr);
     delayMicroseconds(micros);
     return false;
     }
