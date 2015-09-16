aad28263288fd881789cf0a878a2a00a60826db6
Mark Grebe <mark.grebe@ku.edu>
2015-09-16 07:25:41 -0500

    Rename extend op to <<=

--- System/Hardware/Haskino/Data.hs
+++ System/Hardware/Haskino/Data.hs
@@ -314,40 +314,42 @@ modifyRemoteRef32 r f = Command $ ModifyRemoteRef32 r f
 
 -- ToDo: Readd servo and stepper functions
 
+infixr 1 <<=
+
 class RemoteReference a where
     newRemoteRef          :: Expr a -> Arduino (RemoteRef a)
     readRemoteRef         :: RemoteRef a -> Arduino (Expr a)
     writeRemoteRef        :: RemoteRef a -> Expr a -> Arduino ()
     modifyRemoteRef       :: RemoteRef a -> (Expr a -> Expr a) -> Arduino ()
-    extend                :: (Expr a -> Arduino ()) -> Arduino (Expr a) -> Arduino ()
+    (<<=)                 :: (Expr a -> Arduino ()) -> Arduino (Expr a) -> Arduino ()
 
 instance RemoteReference Bool where
     newRemoteRef = newRemoteRefB
     readRemoteRef = readRemoteRefB
     writeRemoteRef = writeRemoteRefB
     modifyRemoteRef = modifyRemoteRefB
-    extend = extendB
+    (<<=) = extendB
 
 instance RemoteReference Word8 where
     newRemoteRef = newRemoteRef8
     readRemoteRef = readRemoteRef8
     writeRemoteRef = writeRemoteRef8
     modifyRemoteRef = modifyRemoteRef8
-    extend = extend8
+    (<<=) = extend8
 
 instance RemoteReference Word16 where
     newRemoteRef = newRemoteRef16
     readRemoteRef = readRemoteRef16
     writeRemoteRef = writeRemoteRef16
     modifyRemoteRef = modifyRemoteRef16
-    extend = extend16
+    (<<=) = extend16
 
 instance RemoteReference Word32 where
     newRemoteRef = newRemoteRef32
     readRemoteRef = readRemoteRef32
     writeRemoteRef = writeRemoteRef32
     modifyRemoteRef = modifyRemoteRef32
-    extend = extend32
+    (<<=) = extend32
 
 data Control =
       Loop (Arduino ())
--- System/Hardware/Haskino/SamplePrograms/ExampleE.hs
+++ System/Hardware/Haskino/SamplePrograms/ExampleE.hs
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 -- |
--- Module      :  System.Hardware.Haskino.SamplePrograms.ButtonE
+-- Module      :  System.Hardware.Haskino.SamplePrograms.ExampleE
 --                Based on System.Hardware.Arduino
 -- Copyright   :  (c) University of Kansas
 -- License     :  BSD3
@@ -9,14 +9,14 @@
 -- Based on our initial simple example
 -------------------------------------------------------------------------------
 
-module System.Hardware.Haskino.SamplePrograms.ButtonE where
+module System.Hardware.Haskino.SamplePrograms.ExampleE where
 
 import System.Hardware.Haskino
 import Data.Boolean
 import Data.Word
 
-buttonE :: IO ()
-buttonE = withArduino False "/dev/cu.usbmodem1421" $ do
+exampleE :: IO ()
+exampleE = withArduino False "/dev/cu.usbmodem1421" $ do
            let button = lit 2
            let led1 = lit 6
            let led2 = lit 7
@@ -24,8 +24,7 @@ buttonE = withArduino False "/dev/cu.usbmodem1421" $ do
            setPinModeE button INPUT
            setPinModeE led1 OUTPUT
            setPinModeE led2 OUTPUT
-           while (lit True) $ do extend (writeRemoteRef x) (digitalReadE button)
-                                 --writeEffectRemoteRef x (digitalReadE button)
+           while (lit True) $ do writeRemoteRef x  <<= digitalReadE button
                                  ex <- readRemoteRef x
                                  digitalWriteE led1 ex
                                  digitalWriteE led2 (notB ex)
