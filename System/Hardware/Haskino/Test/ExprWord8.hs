ea7c7c33a9d52aa7a7633760562a1fde0bb17501
Mark Grebe <mark.grebe@ku.edu>
2015-11-09 16:40:16 -0600

    Add tests to Expr Word8 QuickCheck

--- System/Hardware/Haskino/Test/ExprWord8.hs
+++ System/Hardware/Haskino/Test/ExprWord8.hs
@@ -18,6 +18,24 @@ import Test.QuickCheck.Monadic
 litEval :: Expr Word8 -> Word8
 litEval (Lit8 w) = w
 
+prop_neg :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Property
+prop_neg c r x = monadicIO $ do
+    let local = negate x
+    remote <- run $ send c $ do
+        writeRemoteRef r $ negate (lit x)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
+prop_sign :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Property
+prop_sign c r x = monadicIO $ do
+    let local = signum x
+    remote <- run $ send c $ do
+        writeRemoteRef r $ signum (lit x)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
 prop_add :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Word8 -> Property
 prop_add c r x y = monadicIO $ do
     let local = x + y
@@ -85,12 +103,77 @@ prop_and c r x y = monadicIO $ do
         return v
     assert (local == litEval remote)
 
+prop_or :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Word8 -> Property
+prop_or c r x y = monadicIO $ do
+    let local = x DB..|. y
+    remote <- run $ send c $ do
+        writeRemoteRef r $ (lit x) .|. (lit y)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
+prop_shiftL :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Word8 -> Property
+prop_shiftL c r x y = monadicIO $ do
+    let local = x `DB.shiftL` (fromIntegral y)
+    remote <- run $ send c $ do
+        writeRemoteRef r $ (lit x) `shiftL` (lit y)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
+prop_shiftR :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Word8 -> Property
+prop_shiftR c r x y = monadicIO $ do
+    let local = x `DB.shiftR` (fromIntegral y)
+    remote <- run $ send c $ do
+        writeRemoteRef r $ (lit x) `shiftR` (lit y)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
+prop_setBit :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Word8 -> Property
+prop_setBit c r x y = monadicIO $ do
+    let local = x `DB.setBit` (fromIntegral y)
+    remote <- run $ send c $ do
+        writeRemoteRef r $ (lit x) `setBit` (lit y)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
+prop_clearBit :: ArduinoConnection -> RemoteRef Word8 -> Word8 -> Word8 -> Property
+prop_clearBit c r x y = monadicIO $ do
+    let local = x `DB.clearBit` (fromIntegral y)
+    remote <- run $ send c $ do
+        writeRemoteRef r $ (lit x) `clearBit` (lit y)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
+prop_from32 :: ArduinoConnection -> RemoteRef Word8 -> Word32 -> Property
+prop_from32 c r x = monadicIO $ do
+    let local = fromIntegral x
+    remote <- run $ send c $ do
+        writeRemoteRef r $ fromIntegralB (lit x)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
+prop_from16 :: ArduinoConnection -> RemoteRef Word8 -> Word16 -> Property
+prop_from16 c r x = monadicIO $ do
+    let local = fromIntegral x
+    remote <- run $ send c $ do
+        writeRemoteRef r $ fromIntegralB (lit x)
+        v <- readRemoteRef r
+        return v
+    assert (local == litEval remote)
+
 main :: IO ()
 main = do
     conn <- openArduino False "/dev/cu.usbmodem1421"
     ref <- send conn $ newRemoteRef 0
-    print "Addition Tests:"
-    quickCheck (prop_add conn ref)
+    print "Negation Tests:"
+    quickCheck (prop_neg conn ref)
+    print "Signum Tests:"
+    quickCheck (prop_sign conn ref)
     print "Subtraction Tests:"
     quickCheck (prop_sub conn ref)
     print "Multiplcation Tests:"
@@ -103,4 +186,18 @@ main = do
     quickCheck (prop_comp conn ref)
     print "Bitwise And Tests:"
     quickCheck (prop_and conn ref)
+    print "Bitwise Or Tests:"
+    quickCheck (prop_or conn ref)
+    print "Shift Left Tests:"
+    quickCheck (prop_shiftL conn ref)
+    print "Shift Right Tests:"
+    quickCheck (prop_shiftR conn ref)
+    print "Set Bit Tests:"
+    quickCheck (prop_setBit conn ref)
+    print "Clear Bit Tests:"
+    quickCheck (prop_clearBit conn ref)
+    print "From Word32 Tests:"
+    quickCheck (prop_from32 conn ref)
+    print "From Word16 Tests:"
+    quickCheck (prop_from16 conn ref)
     closeArduino conn
