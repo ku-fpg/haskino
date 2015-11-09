8036303f27bd3581497344d267a46164841c8302
Mark Grebe <mark.grebe@ku.edu>
2015-11-08 20:55:41 -0600

    Fix Eval commands

--- firmware/HaskinoFirmware/HaskinoExprCmds.cpp
+++ firmware/HaskinoFirmware/HaskinoExprCmds.cpp
@@ -23,7 +23,8 @@ bool parseExprMessage(int size, const byte *msg, CONTEXT *context)
 
 static bool handleEval(int type, int size, const byte *msg, CONTEXT *context)
     {
-    byte *expr = (byte *) &msg[2];
+    byte bind = msg[2];
+    byte *expr = (byte *) &msg[3];
     byte reply[5];
     bool boolResult;
     uint8_t word8Result;
@@ -38,29 +39,29 @@ static bool handleEval(int type, int size, const byte *msg, CONTEXT *context)
             reply[0] = EXPR(EXPR_BOOL,EXPR_LIT);
             boolResult = evalBoolExpr(&expr, context);
             memcpy(&reply[1], &boolResult, sizeof(boolResult));
-            sendReply(1+sizeof(bool), EXP_RESP_EVAL, (const byte *) &reply, NULL, 0);
+            sendReply(1+sizeof(bool), EXP_RESP_EVAL, reply, context, bind);
             break;
         case EXPR_WORD8:
             reply[0] = EXPR(EXPR_WORD8,EXPR_LIT);
             word8Result = evalWord8Expr(&expr, context);
             memcpy(&reply[1], &word8Result, sizeof(word8Result));
-            sendReply(1+sizeof(uint8_t), EXP_RESP_EVAL, (const byte *) &reply, NULL, 0);
+            sendReply(1+sizeof(uint8_t), EXP_RESP_EVAL, reply, context, bind);
             break;
         case EXPR_WORD16:
             reply[0] = EXPR(EXPR_WORD16,EXPR_LIT);
             word16Result = evalWord16Expr(&expr, context);
             memcpy(&reply[1], &word16Result, sizeof(word16Result));
-            sendReply(1+sizeof(uint16_t), EXP_RESP_EVAL, (const byte *) &reply, NULL, 0);
+            sendReply(1+sizeof(uint16_t), EXP_RESP_EVAL, reply, context, bind);
             break;
         case EXPR_WORD32:
             reply[0] = EXPR(EXPR_WORD32,EXPR_LIT);
             word32Result = evalWord32Expr(&expr, context);
-            memcpy(&reply[1], &word16Result, sizeof(word16Result));
-            sendReply(1+sizeof(uint32_t), EXP_RESP_EVAL, (const byte *) &reply, NULL, 0);
+            memcpy(&reply[1], &word32Result, sizeof(word32Result));
+            sendReply(1+sizeof(uint32_t), EXP_RESP_EVAL, reply, context, bind);
             break;
         case EXPR_LIST8:
             listMem = evalList8Expr(&expr, context, &alloc);
-            sendReply(listMem[1], EXP_RESP_EVAL, listMem, NULL, 0);
+            sendReply(listMem[1], EXP_RESP_EVAL, listMem, context, bind);
             if (alloc)
                 free(listMem);
             break;
