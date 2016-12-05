fa6d1bd7d953de7c5bea93a0f98025567234dc3a
Mark Grebe <mark.grebe@ku.edu>
2016-12-04 21:28:40 -0600

    Moved plugin to Haskino directory

---
+++ System/Hardware/Haskino/SamplePrograms/Rewrite/TwoButtonIf.hs
@@ -0,0 +1,118 @@
+{-# OPTIONS_GHC -fplugin=System.Hardware.Haskino.ShallowDeepPlugin #-}
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  System.Hardware.Haskino.SamplePrograms.Strong.Blink
+--                Based on System.Hardware.Arduino
+-- Copyright   :  (c) University of Kansas
+--                System.Hardware.Arduino (c) Levent Erkok
+-- License     :  BSD3
+-- Stability   :  experimental
+--
+-- The /hello world/ of the arduino world, blinking the led.
+-------------------------------------------------------------------------------
+
+module Main where
+
+import Prelude hiding (abs)
+
+import System.Hardware.Haskino
+import Control.Monad
+import Data.Word
+import Data.Boolean
+
+twoButtonProg :: Arduino ()
+twoButtonProg = do
+    let led1 = 13
+    let led2 = 13
+    let button1 = 2
+    let button2 = 3
+    setPinMode led1 OUTPUT
+    setPinMode led2 OUTPUT
+    setPinMode button1 INPUT
+    setPinMode button2 INPUT
+    loop $ do 
+        a <- digitalRead button1
+        b <- digitalRead button2
+        if a || b
+        then do
+          digitalWrite led1 a
+          digitalWrite led2 b
+        else do
+          digitalWrite led1 (not a)
+          digitalWrite led2 (not b)          
+        delayMillis 1000
+
+main :: IO ()
+main = withArduino True "/dev/cu.usbmodem1421" twoButtonProg
+
+{-
+{-# NOINLINE rep #-}
+rep :: Expr a -> a
+rep _ = error "Internal error: repB called"
+
+{-# NOINLINE abs #-}
+abs :: ExprB a => a -> Expr a
+abs w = lit w
+-}
+
+{-# RULES 
+    "digitalRead" [0]
+    forall (p :: Word8).
+    digitalRead p = rep_ <$> (digitalReadE $ abs_ p) 
+  #-}
+
+{-# RULES "digitalWrite" [0]
+    forall (p :: Word8) (b :: Bool).
+    digitalWrite p b
+      =
+    digitalWriteE (abs_ p) (abs_ b)
+  #-}
+
+{-# RULES "pinMode" [0]
+    forall (p :: Word8) m.
+    setPinMode p m
+      =
+    setPinModeE (abs_ p) m
+  #-}
+
+{-# RULES "delayMillis" [0]
+    forall (d :: Word32).
+    delayMillis d
+      =
+    delayMillisE (abs_ d)
+  #-}
+
+{-# RULES "loop" [0]
+    forall (m :: Arduino ()).
+    loop m
+      =
+    loopE m
+  #-}
+{-
+{-# RULES "if-then-else" [0]
+    forall (b :: Bool) (m1 :: Arduino ()) (m2 :: Arduino ()).
+    if b then m1 else m2
+      =
+    ifThenElse (abs_ b) m1 m2
+  #-}
+-}
+{-# RULES "abs-push-or" [0]
+    forall (b1 :: Bool) (b2 :: Bool).
+    abs_ (b1 || b2)
+      =
+    (abs_ b1) ||* (abs_ b2)
+  #-}
+
+{-# RULES "rep-3rd-monad" [0]
+    forall (f :: Arduino (Expr Bool)) (k :: Bool -> Arduino b).
+    rep_ <$> f >>= k 
+      =
+    f >>= k . rep_
+  #-}
+
+{-# RULES "abs-rep-fuse" [1]
+    forall x.
+    abs_(rep_(x))
+      =
+    x
+  #-}
--- System/Hardware/Haskino/ShallowDeepPlugin.hs
+++ System/Hardware/Haskino/ShallowDeepPlugin.hs
@@ -1,4 +1,5 @@
-module ShallowDeepPlugin (plugin) where
+{-# LANGUAGE TemplateHaskell #-}
+module System.Hardware.Haskino.ShallowDeepPlugin (plugin) where
 
 import CoreMonad
 import GhcPlugins
@@ -8,6 +9,7 @@ import SimplEnv
 import SimplUtils
 import Data.Data
 import Data.Typeable
+import DataCon
 import IOEnv 
 import OccName
 import TysPrim
@@ -16,6 +18,8 @@ import Var
 import Control.Monad
 import Control.Monad.Writer
 import Data.List 
+
+import qualified System.Hardware.Haskino
 
 plugin :: Plugin
 plugin = defaultPlugin {
@@ -26,8 +30,9 @@ install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 install _ todo = do
   reinitializeGlobals
   let repLambdaToDo = [CoreDoPluginPass "RepLambda" repLambdaPass]
+  let condToDo = [CoreDoPluginPass "RepLambda" condPass]
   let dumpTodo = [CoreDoPluginPass "DumpPass" dumpPass]
-  return $ rules0Pass : repLambdaToDo ++ [rules1Pass] ++ todo ++ dumpTodo
+  return $ condToDo ++ [rules0Pass] ++ repLambdaToDo ++ [rules1Pass] ++ todo -- ++ dumpTodo
 
 rules0Pass :: CoreToDo
 rules0Pass = CoreDoSimplify 1 SimplMode {
@@ -51,6 +56,12 @@ rules1Pass = CoreDoSimplify 1 SimplMode {
 
 repLambdaPass :: ModGuts -> CoreM ModGuts
 repLambdaPass guts = bindsOnlyPass (mapM repBind) guts
+
+condPass :: ModGuts -> CoreM ModGuts
+condPass guts = do
+  Just ifThenElseName <- thNameToGhcName 'System.Hardware.Haskino.ifThenElse
+  ifThenElseId <- lookupId ifThenElseName
+  bindsOnlyPass (mapM (condBind ifThenElseId)) guts
 
 dumpPass :: ModGuts -> CoreM ModGuts
 dumpPass guts = do
@@ -126,6 +137,9 @@ repExpr e =
 varString :: Id -> String 
 varString = occNameString . nameOccName . Var.varName
 
+nameString :: Name -> String 
+nameString = occNameString . nameOccName
+
 buildId :: String -> Type -> CoreM Id
 buildId varName typ = do
   dunique <- getUniqueM
@@ -190,3 +204,96 @@ subVarExprAlts id esub ((ac, b, a) : as) = do
   a' <- subVarExpr id esub a
   bs' <- subVarExprAlts id esub as
   return $ (ac, b, a') : bs'
+
+condBind :: Id -> CoreBind -> CoreM CoreBind
+condBind ifid bndr@(NonRec b e) = do
+  e' <- condExpr ifid e
+  return (NonRec b e')
+condBind ifid (Rec bs) = do
+  bs' <- condExpr' ifid bs
+  return $ Rec bs'
+
+condBind' :: Id -> [(Id, CoreExpr)] -> CoreM [(Id, CoreExpr)]
+condBind' _ [] = return []
+condBind' ifid ((b, e) : bs) = do
+  e' <- condExpr ifid e
+  bs' <- condBind' ifid bs
+  return $ (b, e') : bs'
+
+condExpr :: Id -> CoreExpr -> CoreM CoreExpr
+condExpr ifid e = do
+  case e of
+    Var v -> return $ Var v
+    Lit l -> return $ Lit l
+    Type ty -> return $ Type ty
+    Coercion co -> return $ Coercion co
+    App e1 e2 -> do
+      e1' <- condExpr ifid e1
+      e2' <- condExpr ifid e2
+      return $ App e1' e2'
+    Lam tb e -> do
+      e' <- condExpr ifid e
+      return $ Lam tb e'
+    Let bind body -> do
+      body' <- condExpr ifid body
+      bind' <- case bind of 
+                  (NonRec v e) -> do
+                    e' <- condExpr ifid e
+                    return $ NonRec v e'
+                  (Rec rbs) -> do
+                    rbs' <- condExpr' ifid rbs
+                    return $ Rec rbs'
+      return $ Let bind' body' 
+{-
+    Case e tb ty alts | show (typeOf ty) == "Arduino ()" -> do
+      e' <- condExpr ifid e
+      alts' <- condExprAlts ifid alts
+      if nameString (getName (exprType e)) == "Bool"
+      then condTransform ifid e' alts'
+      else return $ Case e' tb ty alts'
+-}
+    Case e tb ty alts | show (typeOf ty) == "Arduino ()" -> do
+      e' <- condExpr ifid e
+      alts' <- condExprAlts ifid alts
+      if length alts' == 2 
+      then case alts' of
+        [(ac1, _, _), _] -> do
+          case ac1 of 
+            DataAlt d -> do
+              if nameString (getName d) == "False"
+              then condTransform ifid e' alts'
+              else return $ Case e' tb ty alts'
+            _ -> return $ Case e' tb ty alts'
+      else return $ Case e' tb ty alts'
+    Case e tb ty alts -> do
+      e' <- condExpr ifid e
+      alts' <- condExprAlts ifid alts
+      return $ Case e' tb ty alts'
+    Tick t e -> do
+      e' <- condExpr ifid e
+      return $ Tick t e'
+    Cast e co -> do
+      e' <- condExpr ifid e
+      return $ Cast e' co
+
+condExpr' :: Id -> [(Id, CoreExpr)] -> CoreM [(Id, CoreExpr)]
+condExpr' _ [] = return []
+condExpr' ifid ((b, e) : bs) = do
+  e' <- condExpr ifid e
+  bs' <- condExpr' ifid bs
+  return $ (b, e') : bs'
+
+condExprAlts :: Id -> [GhcPlugins.Alt CoreBndr] -> CoreM [GhcPlugins.Alt CoreBndr]
+condExprAlts _ [] = return []
+condExprAlts ifid ((ac, b, a) : as) = do
+  a' <- condExpr ifid a
+  bs' <- condExprAlts ifid as
+  return $ (ac, b, a') : bs'
+
+condTransform :: Id -> CoreExpr -> [GhcPlugins.Alt CoreBndr] -> CoreM CoreExpr
+condTransform ifid e alts = do
+  case alts of
+    [(_, _, e1),(_, _, e2)] -> do
+      return $ mkCoreApps (Var ifid) [e, e1, e2]
+      -- App (App (App (Var ifid) e) e1) e2
+
